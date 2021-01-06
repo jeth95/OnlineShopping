81c2f7a8f5a6b45d3c3ff009c845222d67ff2249
jeth95 <guejethro9@gmail.com>
2021-01-05 22:20:55 -0500

    Added Model for Cart and Page for Cart

---
+++ OnlineShop/Models/Cart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int CartId { get; set; }
+
+        [ForeignKey("UserId")]
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
